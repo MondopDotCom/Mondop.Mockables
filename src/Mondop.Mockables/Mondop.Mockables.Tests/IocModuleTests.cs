174c13d7601c4d90c82b145c4cf1a719df99ee6a
Marcel Vroone <mvroone@it-actief.nl>
2019-06-07 21:30:07 +0200

    Added IocModule and PublishProfile

---
+++ src/Mondop.Mockables/Mondop.Mockables.Tests/IocModuleTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mondop.Abstractions.IoC;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mondop.Mockables.Tests
+{
+    [TestClass]
+    public class IocModuleTests
+    {
+        private IocModule module;
+        private readonly IIoCContainer mockContainer = Substitute.For<IIoCContainer>();
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            module = new IocModule();
+        }
+
+        [TestMethod]
+        public void CallRegister_ExpectRegistrations()
+        {
+            module.Register(mockContainer);
+
+            mockContainer.Received(1).Register<IFile, FileImplementation>();
+            mockContainer.Received(1).Register<IDirectory, DirectoryImplementation>();
+        }
+    }
+}
---
+++ src/Mondop.Mockables/Mondop.Mockables/IocModule.cs
@@ -0,0 +1,17 @@
+﻿using Mondop.Abstractions.IoC;
+using System;
+using System.Collections.Generic;
+
+namespace Mondop.Mockables
+{
+    public class IocModule : IIoCModule
+    {
+        public List<Type> DependsOn => new List<Type>();
+
+        public void Register(IIoCContainer container)
+        {
+            container.Register<IDirectory, DirectoryImplementation>();
+            container.Register<IFile, FileImplementation>();
+        }
+    }
+}
